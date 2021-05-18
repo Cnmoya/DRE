53c69866d852fe4e6d67587f07b8c5e96a68d014
furcelay <fjurcelay@uc.cl>
2021-05-17 21:28:25 -0400

    cambio nombre de la libreria

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DRE-fjurcelay",
+    name="DRE",
     version="0.0.1",
     author="Cristobal Moya, Felipe Urcelay",
     author_email="fjurcelay@uc.cl",

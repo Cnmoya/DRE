76d004b4cde6775a87a26bce05b08def0b3b74c8
furcelay <fjurcelay@uc.cl>
2021-05-17 22:49:21 -0400

    error en import de colab

--- DRE/colab/__init__.py
+++ DRE/colab/__init__.py
@@ -1,4 +1,4 @@
 from .results import Result, Results
-from .dre_gpu import ModelsCube
+from .dre_gpu import ModelGPU
 
-__all__ = ["Result", "Results", "ModelsCube"]
+__all__ = ["Result", "Results", "ModelGPU"]
